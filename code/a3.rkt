2d90bf8e9b4b279762ebb0cfb51d92a13f287dae
siriusdemon <mory2016@126.com>
2022-03-08 15:10:49 +0800

    start a3

--- code/a3.rkt
+++ code/a3.rkt
@@ -1,4 +1,7 @@
+#lang racket/load
+(load "a2.rkt")
 
+;; Part 1
 (define lex
   (lambda (e bound)
     (match e
@@ -100,3 +103,79 @@
 ;               (lambda 
 ;                 (lambda 
 ;                   (lambda ((((((var 8) (var 7)) (var 6)) (var 2)) h) (var 0))))))))))))
+
+;; Part 2
+
+(define empty-env-fn 
+  (lambda ()
+    (lambda (x) 
+      (error 'empty-env-fn "Variable unbound ~s~n" x))))
+
+(define extend-env-fn
+  (lambda (sym val old-env)
+    (lambda (x)
+      (if (eqv? x sym)
+          val
+          (old-env x)))))
+
+(define apply-env-fn
+  (lambda (env x)
+    (env x)))
+
+(define value-of-fn
+  (lambda (exp env)
+    (match exp
+      [`,b #:when (boolean? b) b]
+      [`,n #:when (number? n)  n]
+      [`,y #:when (symbol? y) (apply-env-fn env y)]
+      [`(if ,test ,conseq ,alt) 
+        (if (value-of-fn test env)
+            (value-of-fn conseq env)
+            (value-of-fn alt env))]
+      )))
+
+(println 
+  (value-of-fn
+   '((lambda (x) (if (zero? x)
+                     #t
+                     #f))
+     0)
+   (empty-env-fn)))
+; #t                  
+(println 
+  (value-of-fn 
+   '((lambda (x) (if (zero? x) 
+                     12 
+                     47)) 
+     0) 
+   (empty-env-fn)))
+; 12    
+(println 
+  (value-of-fn
+   '(let ([y (* 3 4)])
+      ((lambda (x) (* x y)) (sub1 6)))
+   (empty-env-fn)))
+; 60
+(println 
+  (value-of-fn
+   '(let ([x (* 2 3)])
+      (let ([y (sub1 x)])
+        (* x y)))
+   (empty-env-fn)))
+; 30
+(println 
+  (value-of-fn
+   '(let ([x (* 2 3)])
+      (let ([x (sub1 x)])
+        (* x x)))
+   (empty-env-fn)))
+; 25
+(println 
+  (value-of-fn
+   '(((lambda (f)
+        (lambda (n) (if (zero? n) 1 (* n ((f f) (sub1 n))))))
+      (lambda (f)
+        (lambda (n) (if (zero? n) 1 (* n ((f f) (sub1 n)))))))
+     5)
+   (empty-env-fn)))
+; 120
